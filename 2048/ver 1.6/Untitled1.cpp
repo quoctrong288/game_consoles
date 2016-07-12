05b7fc053b4d964dd67570d123acc6279d67d3e7
Quoc Trong Pham <quoctrong.qb@gmail.com>
2016-07-12 16:05:35 +0700

    source code
    2048 ver test

---
+++ 2048/ver 1.6/Untitled1.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; ++i)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; --i)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; ++i)
+#define REPD(i,a) for(int i=(a)-1; i>=0; --i)
+
+#define debug(X)  { cout << #X << " = " << (X) << endl; }
+#define ar1(A,n)  { cout << #A << " = "; FOR(_,1,n) cout << A[_] << ' '; cout << endl; }
+#define ar0(A,n)  { cout << #A << " = "; REP(_,n) cout << A[_] << ' '; cout << endl; }
+
+#define ll long long
+#define sqr(x) ((x) * (x))
+#define SZ(x) ((int) (x).size())
+#define ALL(x) (x).begin(), (x).end()
+#define MS(a,x) (memset(a, x, sizeof(a)))
+using namespace std;
+
+const int MN = 1000011;
+const int inf = 1e9+7;
+
+
+int main() {
+   ios :: sync_with_stdio(0); cin.tie(0);
+   #ifndef ONLINE_JUDGE
+       freopen("input.txt", "r", stdin);
+       //freopen("output.txt", "w", stdout);
+   #endif
+   
+   cin >>
+   
+   
+   
+   return 0;
+}
+
